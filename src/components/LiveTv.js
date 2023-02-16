f1bc5ab17e9662ea7a651479ad0ee95daafc9ffa
darionjoness <101945727+darionjoness@users.noreply.github.com>
2023-02-16 17:18:24 -0600

    updated footer and some

--- src/components/LiveTv.js
+++ src/components/LiveTv.js
@@ -13,7 +13,7 @@ const LiveTv = () => {
             <p className='liveTvInfo'>Make the switch from cable. Get 85+ top channels on Hulu (With Ads) + Live TV with your favorite live sports, news, and events - plus the entire Hulu streaming library. With Unlimited DVR, store Live TV recordings for up to nine months and fast-forward through your DVR content. Access endless entertainment with Disney+ and live sports with ESPN+.</p>
             <p className='restrictions'>Regional restrictions, blackouts and Live TV terms apply. Access content from each service separately. Location data required to watch certain content. Unlimited DVR recording is not available for on-demand shows.</p>
             <a className='viewChannels' href="">View Channels In Your Area<MdArrowRightAlt className='rightArrow' /></a>
-            <a className='chevronDown' href=""><BsChevronDown /></a>
+            <a className='chevronDown' href="#liveSports"><BsChevronDown /></a>
         </div>
     </div>
   )
